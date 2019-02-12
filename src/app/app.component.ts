0def1a9c245ae5b3fb0376ffc1ac21f6ed693b93
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-12 17:58:36 -0200

    Adequando template e usando data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+  url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg';
 }
