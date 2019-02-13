4a015596b7c649a19dc59facf55274c9683783c2
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-13 13:23:24 -0200

    Inbound Properties para <ap-photo> de forma que os valores das propriedades sejam definidas na tag do próprio componente

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'ap-photo',
     templateUrl: 'photo.component.html'
 })
 export class PhotoComponent {
-    description = 'Leão';
-    url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg';
+    @Input() description = '';
+    @Input() url = '';
 }
