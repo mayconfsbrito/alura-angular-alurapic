7adae9343dc19f8fdd40760f0a9bcb228b73ec96
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-13 15:35:42 -0200

    Tipando a API e criando a interface Photo

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Photo } from './photo';
 
 const API = 'http://localhost:3000';
 
@@ -9,6 +10,6 @@ export class PhotoService {
     constructor(private http: HttpClient){}
 
     listFromUser(userName: string) {
-        return this.http.get<Object[]>(API + '/flavio/photos');
+        return this.http.get<Photo[]>(API + '/flavio/photos');
     }
 }
