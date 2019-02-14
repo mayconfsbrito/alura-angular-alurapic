0f88fbd19510e1988b35ef6c9c9306d115fef1fe
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-14 15:40:45 -0200

    Tratando validação de campos required na página de login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [
         SignInComponent
+    ],
+    imports: [
+        ReactiveFormsModule,
+        CommonModule
     ]
 })
 export class HomeModule {}
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './signin.component.html'
 })
-export class SignInComponent { }
+export class SignInComponent implements OnInit {
+
+    loginForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.loginForm = this.formBuilder.group({
+            userName: ['', Validators.required ],
+            password: ['', Validators.required ]
+        });
+    }
+}
