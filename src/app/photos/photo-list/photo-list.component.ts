288cf37e6f815f61fe2353ba21523ca28c239fe4
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-14 14:15:56 -0200

    Output Property - Consertando a Busca
    Criando o evento onTyping, tratando o debounce em SearchComponent e esvaziando
    o campo de busca ao clicar em "loadMore".

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -31,6 +31,7 @@ export class PhotoListComponent implements OnInit {
     this.photoService
       .listFromUserPaginated(this.userName, ++this.currentPage)
       .subscribe(photos => {
+        this.filter = '';
         this.photos = this.photos.concat(photos);
         if (!photos.length) { this.hasMore = false; }
       });
--- src/app/photos/photo-list/search/search.component.ts
+++ src/app/photos/photo-list/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter, Input } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
@@ -9,13 +9,16 @@ import { debounceTime } from 'rxjs/operators';
 })
 export class SearchComponent implements OnInit, OnDestroy {
 
+  @Output() onTyping = new EventEmitter<string>();
+  @Input() value: string = '';
   debounce: Subject<string> = new Subject<string>();
 
   constructor() { }
 
   ngOnInit(): void {
     this.debounce
-      .pipe(debounceTime(300));
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.onTyping.emit(filter));
   }
 
   ngOnDestroy(): void {
