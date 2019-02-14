cd63689c1d63cc6208168d46c9d32f9dfb83e8be
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-14 09:26:01 -0200

    Criando input para pesquisa e adicionando evento de keyup para o novo campo filter para photo-list

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -12,6 +12,7 @@ import { Photo } from '../photo/photo';
 export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
+  filter: string = '';
 
   constructor(
     private photoService: PhotoService,
