750370e2ffa3ce30822480300b00097b77b6edf9
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-14 14:57:14 -0200

    Adaptação da diretiva com mouse hover e brightness

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -8,6 +8,7 @@ import { FilterByDescription } from './filter-by-description.pipe';
 import { PhotoModule } from './../photo/photo.module';
 import { CardModule } from './../../shared/components/card/card.module';
 import { SearchComponent } from './search/search.component';
+import { DarkenOnHoverModule } from './../../shared/directives/darken-on-hover/darken-on-hover.module';
 
 @NgModule({
     declarations: [
@@ -20,7 +21,8 @@ import { SearchComponent } from './search/search.component';
     imports: [
     CommonModule,
         PhotoModule,
-        CardModule
+        CardModule,
+        DarkenOnHoverModule
     ]
 })
 export class PhotoListModule{}
