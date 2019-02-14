9d1261268a1c686cc2635c7c40a60230637388fb
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-14 09:43:12 -0200

    Filtragem de fotos a partir do inputText e mensagem caso a pesquisa não satisfaça

---
+++ src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Photo } from '../photo/photo';
+
+@Pipe({ name: 'filterByDescription'})
+export class FilterByDescription implements PipeTransform {
+
+    transform(photos: Photo[], descriptionQuery: string) {
+        descriptionQuery = descriptionQuery
+            .trim()
+            .toLocaleLowerCase();
+
+        if (descriptionQuery) {
+            return photos.filter(
+                photo => photo.description.toLowerCase().includes(descriptionQuery)
+            );
+        } else {
+            return photos;
+        }
+    }
+
+}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -6,13 +6,15 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 
 @NgModule({
     declarations: [
         PhotoComponent,
         PhotoListComponent,
         PhotoFormComponent,
-        PhotosComponent
+        PhotosComponent,
+        FilterByDescription
     ],
     imports: [
         HttpClientModule,
