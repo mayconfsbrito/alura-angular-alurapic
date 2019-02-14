c27ffd1cebc3594633215edd5d944e3ce4e71c1f
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-14 10:09:10 -0200

    Resolver - Para disponibilizar as fotos para o componente antes dele ser carregado.

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,11 +4,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 
 const routes: Routes = [
-    { path: 'user/:username', component: PhotoListComponent },
-    { path: 'p/Add', component: PhotoFormComponent },
-    { path: '**', component: NotFoundComponent }
+    {
+        path: 'user/:userName',
+        component: PhotoListComponent,
+        resolve: {
+            photos: PhotoListResolver
+        }
+    },
+    {
+        path: 'p/Add',
+        component: PhotoFormComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
 ];
 
 @NgModule({
--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
 
 @Component({
-  selector: 'app-photo-list',
+  selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.scss']
 })
@@ -14,19 +13,10 @@ export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter: string = '';
 
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) {}
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    const userName = this.activatedRoute.snapshot.params.username;
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
-
+    this.photos = this.activatedRoute.snapshot.data.photos;
   }
 
 }
