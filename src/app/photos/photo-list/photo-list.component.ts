c5d0e7bd5756c0eafc04dab108d215a0c54634ba
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-14 10:21:47 -0200

    Debounce - Aplicando Subscribe ao alterar o Input para atrasar a filtragem e não realizar várias buscas/requisições a cada dígito inserido

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 
@@ -8,15 +10,22 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.scss']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
   }
 
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
 }
