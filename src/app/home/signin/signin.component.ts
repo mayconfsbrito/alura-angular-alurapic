4d26703575d06eb9c6ac29e4af9156a99dc277a7
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-15 10:26:32 -0200

    Criando Serviço de Verificação de Plataforma (no Browser) e executando focus em caso de login inválido

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { AuthService } from './../../core/auth.service';
+import { AuthService } from '../../core/auth/auth.service';
 import { Router } from '@angular/router';
+import { PlatformDetectorService } from './../../core/platform-detector/platform-detetctor.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -14,7 +15,8 @@ export class SignInComponent implements OnInit {
     constructor(
         private formBuilder: FormBuilder,
         private authService: AuthService,
-        private router: Router
+        private router: Router,
+        private platformDetectorService: PlatformDetectorService
     ) {}
 
     ngOnInit(): void {
@@ -35,7 +37,8 @@ export class SignInComponent implements OnInit {
                 () => this.router.navigate(['user', userName]),
                 err => {
                     console.log(err);
-                    this.userNameInput.nativeElement.focus();
+                    this.platformDetectorService.isPlatformBrowser() &&
+                        this.userNameInput.nativeElement.focus();
                     this.loginForm.reset();
                     alert('Invalid user name or password!');
                 }
