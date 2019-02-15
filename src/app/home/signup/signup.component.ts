767c87e27750d68415fb5bf7e82e70092119ed0c
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-15 16:49:18 -0200

    Criação de Validator personalizado para lowercase

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { lowerCaseValidator } from 'src/app/shared/lower-case.validator';
 
 @Component({
   templateUrl: './signup.component.html',
@@ -31,7 +32,7 @@ export class SignupComponent implements OnInit {
         '',
         [
           Validators.required,
-          Validators.pattern(/^[a-z0-9_\-]+$/),
+          lowerCaseValidator,
           Validators.minLength(2),
           Validators.maxLength(30)
         ]
