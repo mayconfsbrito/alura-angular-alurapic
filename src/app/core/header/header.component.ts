023df2b4b539026f0ede071a75e8068874676e60
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-15 13:42:33 -0200

    Criação do Serviço Usuário para armazenamento e decode da Hash JWT de autenticação

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
+import { UserService } from './../user/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
 
 @Component({
     selector: 'ap-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+    user$: Observable<User>;
+    user: User;
+
+    constructor(userService: UserService) {
+        this.user$ = userService.getUser();
+        this.user$.subscribe(user => this.user = user);
+    }
+}
---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Subject, BehaviorSubject } from 'rxjs';
+import { TokenService } from './../token/token.service';
+import { Injectable } from '@angular/core';
+import * as jwt_decode from 'jwt-decode';
+import { User } from './user';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+
+    private userSubject = new BehaviorSubject<User>(null);
+
+    constructor(private tokenService: TokenService) {
+        this.tokenService.hasToken() &&
+            this.decodeAndNotify();
+    }
+
+    setToken(token: string) {
+        this.tokenService.setToken(token);
+        this.decodeAndNotify();
+    }
+
+    getUser() {
+        return this.userSubject.asObservable();
+    }
+
+    private decodeAndNotify() {
+        const token = this.tokenService.getToken();
+        const user = jwt_decode(token) as User;
+        this.userSubject.next(user);
+    }
+}
