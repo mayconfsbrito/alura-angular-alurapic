833ab7063819addcc5bcca9157001fec67093f78
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-15 10:34:20 -0200

    Login - Executando pipe após o login para capturar o token de acesso do usuário

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000';
 
@@ -12,6 +13,15 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   authenticate(userName: string, password: string): Observable {
-    return this.http.post(API_URL + '/user/login', {userName, password });
+    return this.http
+      .post(
+        API_URL + '/user/login',
+        {userName, password },
+        { observe: 'response'}
+      )
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        console.log(authToken);
+      }));
   }
 }
