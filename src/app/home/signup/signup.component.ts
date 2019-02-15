8ba98926f03a01ff2cf723bd937fc6b2548a807f
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-15 16:36:40 -0200

    Alterando componente SignUp para exibir validações do formulário

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,11 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      fullName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ]
+      ],
+      userName: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[a-z0-9_\-]+$/),
+          Validators.minLength(2),
+          Validators.maxLength(30)
+        ]
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(14)
+        ]
+      ],
+    });
+  }
 
 }
