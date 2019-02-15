b2c552c48e3b8e1ffea7dccaede8132d0f91fc3b
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-15 13:07:00 -0200

    Criando componente de Header da página

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+    declarations: [
+        HeaderComponent
+    ],
+    exports: [
+        HeaderComponent
+    ]
+})
+export class CoreModule {}
