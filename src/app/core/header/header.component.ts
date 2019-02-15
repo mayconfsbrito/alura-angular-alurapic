f32efd657cbcced0f7aba5f1c9cc18f2c5bb2e41
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-15 14:01:46 -0200

    LogOut

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { UserService } from './../user/user.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
 import { User } from '../user/user';
+import { UserService } from './../user/user.service';
 
 @Component({
     selector: 'ap-header',
@@ -11,7 +13,15 @@ export class HeaderComponent {
 
     user$: Observable<User>;
 
-    constructor(userService: UserService) {
+    constructor(
+        private userService: UserService,
+        private router: Router
+    ) {
         this.user$ = userService.getUser();
+    }
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['']);
     }
 }
--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -23,6 +23,11 @@ export class UserService {
         return this.userSubject.asObservable();
     }
 
+    logout() {
+        this.tokenService.removeToken();
+        this.userSubject.next(null);
+    }
+
     private decodeAndNotify() {
         const token = this.tokenService.getToken();
         const user = jwt_decode(token) as User;
