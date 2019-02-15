92d56aade0f1a91a8e28f244a905996517d9de76
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-15 11:38:25 -0200

    Criando Service do Token para persistir no LocalStorage

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+
+import { TokenService } from '../token/token.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -10,17 +11,21 @@ const API_URL = 'http://localhost:3000';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService
+  ) { }
 
-  authenticate(userName: string, password: string): Observable {
+  authenticate(userName: string, password: string) {
     return this.http
       .post(
         API_URL + '/user/login',
-        {userName, password },
+        { userName, password },
         { observe: 'response'}
       )
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken);
         console.log(authToken);
       }));
   }
