e8c381d8be0d48ce4de42203a979e9bfeb12d39a
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-18 13:02:20 -0300

    Adaptando para Exibição de photo cujo foi realizado upload e está armazenada na api

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,10 +1,27 @@
 import { Component, Input } from '@angular/core';
 
+const CLOUD = 'http://localhost:3000/imgs/';
+
 @Component({
     selector: 'ap-photo',
     templateUrl: 'photo.component.html'
 })
 export class PhotoComponent {
+
+    private _url = '';
+
     @Input() description = '';
-    @Input() url = '';
+
+    @Input() set url(url: string) {
+
+        if (!url.startsWith('data')) {
+            this._url = CLOUD + url;
+        } else {
+            this._url = url;
+        }
+    }
+
+    get url() {
+        return this._url;
+    }
 }
