5724f0740d8833925051bf179e67195f838db235
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-18 13:29:03 -0300

    Upload - Exibindo preview da imagem a ser subida ao selecioná-la.

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -12,6 +12,7 @@ export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
   file: File;
+  preview: string;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -33,6 +34,13 @@ export class PhotoFormComponent implements OnInit {
     this.photoService
       .upload(description, allowComments, this.file)
       .subscribe(() => this.router.navigate(['']));
+  }
+
+  handleFile(file: File) {
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event: any) => this.preview = event.target.result;
+    reader.readAsDataURL(file);
   }
 
 }
--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -5,6 +5,7 @@ import { PhotoFormComponent } from './photo-form.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { VMessageModule } from '../../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { PhotoModule } from '../photo/photo.module';
 
 @NgModule({
     declarations: [
@@ -15,7 +16,8 @@ import { RouterModule } from '@angular/router';
         ReactiveFormsModule,
         VMessageModule,
         FormsModule,
-        RouterModule
+        RouterModule,
+        PhotoModule
     ]
 })
 export class PhotoFormModule { }
