d920a9ad3b3934ff7aba292cb00f694d1e3b1895
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-18 10:52:14 -0300

    Criando Formulário de Fotos e alterando o componente photo-form.component

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
         }
     },
     {
-        path: 'p/Add',
+        path: 'p/add',
         component: PhotoFormComponent
     },
     {
--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PhotoFormComponent } from './photo-form.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { VMessageModule } from '../../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [
         PhotoFormComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        FormsModule
     ]
 })
 export class PhotoFormModule { }
