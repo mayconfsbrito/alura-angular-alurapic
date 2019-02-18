9d75a01a2193eb23ca684f3e12b136cc4a7b5b96
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-18 11:27:40 -0300

    Obtendo dados do formulário de upload de foto

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -9,6 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
+  file: File;
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -18,6 +19,12 @@ export class PhotoFormComponent implements OnInit {
       description: ['', Validators.maxLength(300)],
       allowComments: [true]
     });
+  }
+
+  upload() {
+    const description = this.photoForm.get('description').value;
+    const allowComments = this.photoForm.get('allowComments').value;
+    console.log(description, allowComments, this.file);
   }
 
 }
