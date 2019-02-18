2b4fbad925520e537501b21641678bd56ec7ad17
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-18 10:24:50 -0300

    Implementando interceptador para buscar o token automaticamente a cada requisição

---
+++ src/app/core/auth/request.interceptor.ts
@@ -0,0 +1,24 @@
+import { HttpInterceptor, HttpHandler, HttpEvent, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TokenService } from './../token/token.service';
+
+@Injectable()
+export class RequestInterceptor implements HttpInterceptor {
+
+    constructor(private tokenService: TokenService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        if (this.tokenService.hasToken()) {
+            const token = this.tokenService.getToken();
+            req = req.clone({
+                setHeaders: {
+                    'x-access-token': token
+                }
+            })
+        }
+        return next.handle(req);
+    }
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
-
-import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+import { HeaderComponent } from './header/header.component';
+import { RequestInterceptor } from './auth/request.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
     declarations: [
         HeaderComponent
     ],
     imports: [
-        CommonModule,
+    CommonModule,
         RouterModule
     ],
     exports: [
         HeaderComponent
+    ],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: RequestInterceptor,
+            multi: true
+        }
     ]
 })
 export class CoreModule {}
