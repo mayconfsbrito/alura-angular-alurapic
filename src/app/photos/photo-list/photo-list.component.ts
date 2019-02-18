bb3a3f575d7e9828f860a1616a73ea60c5594694
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-18 17:35:33 -0300

    Subscrevendo Observable ao iniciar um photo-list, pois ao alterar a visualização das fotos listadas para cada usuário, a página não estava sendo atualizada.

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -23,8 +23,12 @@ export class PhotoListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.userName = this.activatedRoute.snapshot.params.userName;
-    this.photos = this.activatedRoute.snapshot.data.photos;
+    this.activatedRoute.params.subscribe(
+      params => {
+        this.userName = this.activatedRoute.snapshot.params.userName;
+        this.photos = this.activatedRoute.snapshot.data.photos;
+      }
+    );
   }
 
   load() {
