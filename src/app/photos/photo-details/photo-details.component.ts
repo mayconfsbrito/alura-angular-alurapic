e62219b4c593e2ad9d77a677fe5da7b94ea828ac
Maycon Fernando Silva Brito <maycon.fernando@totvs.com.br>
2019-02-20 12:58:34 -0300

    Removendo a rota da history API

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -40,7 +40,7 @@ export class PhotoDetailsComponent implements OnInit {
             .removePhoto(this.photoId)
             .subscribe(() => {
                 this.alertService.success("Photo removed", true);
-                this.router.navigate(['/user', this.userService.getUserName()]);
+                this.router.navigate(['/user', this.userService.getUserName()], { replaceUrl: true });
             },
             err => {
                 console.log(err);
